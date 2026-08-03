804a371bfdc944eb6185b02421792c6ab0316adf
NicanorCellati <garrotino28@gmail.com>
2026-08-02 21:21:52 -0300

    Actualizo dependencia en app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
-import { provideInMemoryWebApi } from './core/services/provide-in-memory-web-api';
+import { provideInMemoryWebApi } from './core/services/in-memory-data/provide-in-memory-web-api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([loadingInterceptor])),
     provideInMemoryWebApi(),
